39d0143b1f94771df32266527225bf0d31966acd
Henok <49408901+henokcreative@users.noreply.github.com>
2025-12-12 11:59:34 +0200

    fixed normet link

--- src/pages/WebdesignPage.jsx
+++ src/pages/WebdesignPage.jsx
@@ -20,7 +20,7 @@ const WebdesignPage = () => {
       title: "Nordic Metabolomics Society",
       subtitle: "Brand identity, webpage design and development",
       imgUrl: "assets/webdesignDev/thumb-nordmet.png",
-      link: "https://nordmet.org",
+      link: "https://nordmetsoc.org",
     },
     {
       title: "Turku PET Centre",
