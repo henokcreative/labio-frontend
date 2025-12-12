5eaa7449d8f4ec9b719ab2983d675be6e13c79c4
Henok <49408901+henokcreative@users.noreply.github.com>
2025-12-12 11:58:42 +0200

    fixed card about page - 2

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -2,14 +2,29 @@ import React from "react";
 import "./Hero.css";
 
 const Hero = ({ title, subheading, subtitle, ctaText, ctaLink }) => {
+
+  const smoothScroll = (e) => {
+    e.preventDefault();
+    const target = document.querySelector(ctaLink);
+    if (target) {
+      target.scrollIntoView({
+        behavior: "smooth",
+        block: "start",
+      });
+    }
+  };
+
   return (
     <section className="hero">
       <div className="hero-content">
         <h1>{title}</h1>
         <h4>{subheading}</h4>
         <p>{subtitle}</p>
+
         {ctaText && ctaLink && (
-          <a href={ctaLink} className="btn button">{ctaText}</a>
+          <a href={ctaLink} onClick={smoothScroll} className="btn button">
+            {ctaText}
+          </a>
         )}
       </div>
     </section>
