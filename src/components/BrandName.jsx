156d74b6451ddb7aa18b73f7b8cc143fe7d798ee
Henok <49408901+henokcreative@users.noreply.github.com>
2026-02-10 15:34:02 +0200

    Brand name -2

--- src/components/BrandName.jsx
+++ src/components/BrandName.jsx
@@ -1,7 +1,7 @@
 // components/BrandName.jsx
 const BrandName = () => (
   <span className="brand-name">
-    L<span className="brand-accent">A</span>B<span className="brand-accent">io</span> MEDIA
+    L<span className="brand-accent">A</span>B<span className="brand-accent">io</span> M<span className="brand-accent">edia</span>
   </span>
 );
 
